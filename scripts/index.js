e71ff30e96006b0129197859f694ba430a808796
Maksim Semenov <maksimuma@list.ru>
2022-11-06 01:25:16 +0300

    hotfix: первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,21 @@
 const editElem = document.querySelector('.profile__edit-button');
 const popupElem = document.querySelector('.popup');
 const popupCloseElem = popupElem.querySelector('.popup__exit-button');
-
-editElem.addEventListener('click', () => {
-  popupElem.classList.add('popup_opened');
-});
-
-popupCloseElem.addEventListener('click', () => {
-  popupElem.classList.remove('popup_opened');
-});
-
 const formProfile = popupElem.querySelector('.form-profile');
 const nameInput = popupElem.querySelector('.form-profile__input_type_name');
 const statusInput = popupElem.querySelector('.form-profile__input_type_status');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+
+function popupOpen() {
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
+  popupElem.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popupElem.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -20,11 +23,13 @@ function formSubmitHandler (evt) {
   let editedName = nameInput.value;
   let editedStatus = statusInput.value;
 
-  let profileName = document.querySelector('.profile__name');
-  let profileStatus = document.querySelector('.profile__status');
-
   profileName.textContent = editedName;
   profileStatus.textContent = editedStatus;
+
+  popupClose();
 }
 
-formProfile.addEventListener('submit', formSubmitHandler); 
+editElem.addEventListener('click', popupOpen);
+popupCloseElem.addEventListener('click', popupClose);
+
+formProfile.addEventListener('submit', formSubmitHandler);
